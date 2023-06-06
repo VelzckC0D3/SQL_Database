5b936ea742ce3a8a6b5b17daca5be10b7e47f322
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-06-06 15:37:28 -0500

    SQL Set Up
    - added a scheme for the database on the schema.sql
    - inserted data to the database on data.sql
    - inserted query selectors on queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon'; /* selected animals whose name ends with 'mon' */
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31'; /* selected animals born between 2016 and 2019 */
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3; /* selected animals that are neutered and have less than 3 escape attempts */
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu'; /* selected date of birth of Agumon and Pikachu */
+SELECT name, escape_attempts FROM animals WHERE weight > 10.5; /* selected name and escape attempts of animals that weight more than 10.5 */
+SELECT * FROM animals WHERE neutered = true; /* selected animals that are neutered */
+SELECT * FROM animals WHERE name <> 'Gabumon'; /* selected animals that are not Gabumon */
+SELECT * FROM animals WHERE weight BETWEEN 10.4 AND 17.3; /* selected animals that weight between 10.4 and 17.3 */
--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempts INT NOT NULL DEFAULT 0,
+	neutered BOOLEAN DEFAULT false,
+	wight FLOAT NOT NULL DEFAULT 0
 );
