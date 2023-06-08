53011513bed38cbc7d698e918c578298b18c0545
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-06-08 17:45:50 -0500

    New Tables & Schema
    - added 2 new tables "owners" & "species"
    - added the schema for the new tables so they are referenced on "animals"
    - replaced the column "species" to the "animals" table with "species_id" referencing the new table
    - added a new column to "animals" called "owners_id" referencing the new table

--- schema.sql
+++ schema.sql
@@ -13,3 +13,18 @@ CREATE TABLE animals (
 
 ALTER TABLE animals /* select the table to be altered */
 ADD COLUMN species VARCHAR; /* add a new column to the table */
+
+CREATE TABLE owners ( /* create a new table */
+	id SERIAL PRIMARY KEY,
+	full_name VARCHAR(50) NOT NULL,
+	age INT NOT NULL
+);
+
+CREATE TABLE species (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE animals ADD species_id int REFERENCES species(id); /* add a foreign key to the table */
+ALTER TABLE ANIMALS ADD owners_id int REFERENCES owners(id); /* add a foreign key to the table */
+
