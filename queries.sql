bdf63afee5dfeb82475616586c4f80b7f2f09d2e
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-06-07 22:42:18 -0500

    Updated queries
    - added all the requested queries, the screenshots will be provided on the PR

--- queries.sql
+++ queries.sql
@@ -8,3 +8,54 @@ SELECT name, escape_attempts FROM animals WHERE weight > 10.5; /* selected name 
 SELECT * FROM animals WHERE neutered = true; /* selected animals that are neutered */
 SELECT * FROM animals WHERE name <> 'Gabumon'; /* selected animals that are not Gabumon */
 SELECT * FROM animals WHERE weight BETWEEN 10.4 AND 17.3; /* selected animals that weight between 10.4 and 17.3 */
+
+/* Set a begin, then update the species of all animals to 'unspecified' and rollback the changes. */
+
+BEGIN; /* begin the transaction */
+
+UPDATE Animals /* select which table to update */
+SET species = 'unspecified'; /* set the species of all animals to 'unspecified' */
+
+ROLLBACK; /* rollback the changes */
+
+BEGIN;
+
+UPDATE Animals 
+SET species = 'Digimon' /* set the species of all animals to 'Digimon' */
+WHERE name LIKE '%mon'; /* select animals whose name ends with 'mon' */
+
+UPDATE Animals
+SET species = 'Pokemon' /* set the species of all animals to 'Pokemon' */
+WHERE species IS NULL; /* select animals whose species is null */
+
+COMMIT; /* commit the changes */
+
+BEGIN; /* begin the transaction */
+
+DELETE FROM Animals; /* delete all animals */
+
+ROLLBACK; /* rollback the changes */
+
+BEGIN;
+
+DELETE FROM Animals 
+WHERE date_of_birth > '2022-01-01'; /* delete animals born after 2022 */
+
+SAVEPOINT checkpoint; /* create a savepoint */
+
+UPDATE Animals
+SET weight = weight * -1; /* set the weight of all animals to negative */
+
+ROLLBACK TO checkpoint; /* rollback the changes to the savepoint */
+
+UPDATE Animals
+SET weight = weigh * -1; /* set the weight of all animals to negative */
+WHERE weight < 0; /* select animals whose weight is negative */
+
+COMMIT; /* commit the changes */
+
+SELECT COUNT(*) FROM Animals; /* count the number of animals */
+SELECT COUNT(*) FROM Animals WHERE escape_attempts > 0; /* count the number of animals that have '0' escape attempts */
+SELECT AVG(weight) FROM Animals;  /* calculate the average weight of all animals */
+SELECT species, MIN(weight), MAX(weight) FROM Animals GROUP BY species; /* calculate the minimum and maximum weight of each species */
+SELECT species, AVG(escape_attempts) FROM Animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species; /* calculate the average escape attempts of animals born between 1990 and 2000 */
