c34bdf1729cfdbc4e47bf46aac54593098cbb6d3
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-06-07 22:41:57 -0500

    Update schema
    - added a new column to the table named 'species'

--- schema.sql
+++ schema.sql
@@ -1,10 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY, /* Primary key that identifies each object in the table. */
 	name varchar(50) NOT NULL,
 	date_of_birth DATE NOT NULL,
 	escape_attempts INT NOT NULL DEFAULT 0,
 	neutered BOOLEAN DEFAULT false,
 	wight FLOAT NOT NULL DEFAULT 0
 );
+
+/* Add a new column to the table */
+
+ALTER TABLE animals /* select the table to be altered */
+ADD COLUMN species VARCHAR; /* add a new column to the table */
