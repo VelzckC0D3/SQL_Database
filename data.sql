eab598126c812cca75483bb999df9e62c3eb46eb
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-06-07 22:40:48 -0500

    Update animals table
    - added the provided animals to the table with the required data

--- data.sql
+++ data.sql
@@ -6,3 +6,15 @@ VALUES
 	('Gabumon', '2018-11-15', 8, true, 2),
 	('Pikachu', '2021-01-07', 15.04, false, 1),
 	('Devimon', '2017-05-12', 11, true, 5);
+
+/* Add new data to the table */
+
+INSERT INTO animals (name, date_of_birth, weight, neutered, escape_attempts)
+VALUES
+	('Charmander', '2020-02-08', -11, false, 0),
+	('Plantmon', '2021-11-15', -5.7, true, 2),
+	('Squirtle', '1993-04-02', -12.13, false, 3),
+	('Angemon', '2005-06-12', -45, true, 1),
+	('Boarmon', '2005-06-07', 20.4, true, 7),
+ 	('Blossom', '1998-10-13', 17, true, 3),
+	('Ditto', '2022-05-14', 22, true, 4);
